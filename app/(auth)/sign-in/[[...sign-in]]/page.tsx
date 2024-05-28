8389c7804a7c6a278f770b04c59cb7bc7fe9609e
Tejas Chaudhari <tejasc888@gmail.com>
2024-05-28 09:56:25 +0530

    sign-in component is now in center

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="mx-auto max-w-min mt-16">
+      <SignIn />
+    </div>
+  );
 }
